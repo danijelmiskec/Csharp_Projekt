98eaeb54a2f100080448b123b8973583a41e5285
Danijel Miškec <dmiskec@vub.hr>
2023-05-18 15:54:45 +0200

    inicijalni commit

--- Projekt/Properties/AssemblyInfo.cs
+++ Projekt/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NotesApp")]
+[assembly: AssemblyTitle("Projekt")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NotesApp")]
+[assembly: AssemblyProduct("Projekt")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("85bf892f-5f25-4e46-9e60-7772d28b7dbc")]
+[assembly: Guid("c826559b-1aee-4ded-966d-6b5412eb660e")]
 
 // Version information for an assembly consists of the following four values:
 //
